ae998e74815c2b62920adeda19ebc0b82bb7e28c
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-05-16 19:06:31 -0700

    feat: divide and conquer example

--- problem_solving.js
+++ problem_solving.js
@@ -52,3 +52,24 @@ function maxSubArraySum(arr, num) {
     }
     return maxSum
 }
+
+function search(arr, val) {
+    let min = 0
+    let max = arr.length - 1
+
+    while (min <= max) {
+        let middle = Math.floor((min + max) / 2)
+        let currentEl = arr[middle]
+
+        if (currentEl < val) {
+            min = middle + 1
+        }
+        else if (currentEl > val) {
+            max = middle - 1
+        }
+        else {
+            return middle
+        }
+    }
+    return -1
+}
