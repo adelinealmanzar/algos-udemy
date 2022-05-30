275cf4007bd2412da6028299412e3e79fb857b55
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-05-30 14:06:22 -0700

    feat: bst find

--- data_structures.js
+++ data_structures.js
@@ -408,6 +408,23 @@ class BinarySearchTree {
 			}
 		}
 	}
+
+	find(val) {
+		if (!this.root) return false
+
+		let current = this.root
+		let found = false
+		while (current && !found) {
+			if (val < current.val) {
+				current = current.left
+			} else if (val > current.val) {
+				current = current.right
+			} else {
+				found = true
+			}
+		}
+		return found
+	}
 }
 
 
