414965875617e93ed6c5a603b1b2d189f503f4fd
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-05-31 14:38:58 -0700

    feat: BST BFS

--- data_structures.js
+++ data_structures.js
@@ -425,6 +425,31 @@ class BinarySearchTree {
 		}
 		return found
 	}
+
+	BFS() {
+		let visited = []
+		let queue = []
+		let node = this.root
+
+		queue.push(node)
+
+		while (queue.length) {
+			node = queue.shift()
+			visited.push(node.val)
+			if (node.left) queue.push(node.left)
+			if (node.right) queue.push(node.right)
+		}
+
+		return visited
+	}
 }
 
+const tree = new BinarySearchTree()
+tree.insert(10)
+tree.insert(6)
+tree.insert(15)
+tree.insert(3)
+tree.insert(8)
+tree.insert(20)
 
+console.log(tree.BFS())
